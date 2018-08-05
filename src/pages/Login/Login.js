e9bf7084b3548aba606f1605cb6078e17d7d74a7
JesBrian <jesbrian666@gmail.com>
2018-08-05 10:59:32 +0800

    axios - [ 二次封装 ]

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -10,6 +10,7 @@ import {
 import NormalNavbar from '../../layout/Top/type/NormalNavbar.js'
 import SuperButton from '../../components/SuperButton.js'
 
+import { phoneLogin } from '../../utils/http/request/login.js'
 
 export default class Login extends React.Component {
   constructor (props) {
@@ -26,12 +27,32 @@ export default class Login extends React.Component {
         <NormalNavbar navigation={this.props.navigation} label={'手机号登录'} />
 
         <View style={{flex:1, flexDirection:'column', alignItems:'center'}}>
-          <TextInput />
-          <TextInput />
-          <SuperButton label="登录" width={Dimensions.get('window').width - 88} height={53} onPressEvent={() => this.props.navigation.navigate('Login')}/>
+          <TextInput value={this.state.phone} onChangeText={(phone) => {
+            this.setState({
+              phone: phone
+            })
+          }} style={{width:Dimensions.get('window').width - 88, height:53}} />
+          <TextInput value={this.state.passwd} secureTextEntry={true} onChangeText={(passwd) => {
+            this.setState({
+              passwd: passwd
+            })
+          }} style={{width:Dimensions.get('window').width - 88, height:53}} />
+          <SuperButton label="登录" width={Dimensions.get('window').width - 88} height={53} onPressEvent={this.phoneLogin.bind(this)} />
         </View>
       </View>
     );
+  }
+
+
+  phoneLogin () {
+    phoneLogin({
+      phone: this.state.phone,
+      passwd: this.state.passwd
+    }).then( res => {
+      alert(res.msg)
+    }).catch( error => {
+      alert(error)
+    })
   }
 }
 
--- src/util/http/request/login.js
+++
@@ -1,6 +0,0 @@
-import http from '../http.js' // 导入封装的 axios 对象
-import { API_PATH } from '../apis.js' // 导入封装的 apis 对象
-
-export function phoneLogin(params = {}) {
-  return http.post(API_PATH.phoneLogin, params)
-}
