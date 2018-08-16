6d503f9505d5eee425e2899c374df4e14b1d5d65
JesBrian <jesbrian666@gmail.com>
2018-08-16 22:57:49 +0800

    播放列表 - [ update ]

--- src/layout/Bottom/NowPlayList.js
+++ src/layout/Bottom/NowPlayList.js
@@ -16,6 +16,7 @@ export default class NowPlayList extends React.Component {
   constructor (props) {
     super(props);
     this.state = {
+      playModel: 'loop'
     };
   }
 
@@ -28,28 +29,41 @@ export default class NowPlayList extends React.Component {
           <View />
         </TouchableOpacity>
         <View style={{height:338, position:'relative', flex:1, flexDirection:'column'}}>
-          <View style={{width:'100%', height:48, flexDirection:'row', borderTopLeftRadius:12, borderTopRightRadius:12, backgroundColor:'#77ffac'}}>
-            <TouchableOpacity>
-              <View style={{width:138, height:'100%', flexDirection:'row', justifyContent:'center', alignItems:'center', borderTopLeftRadius:12}}>
-                <Text style={[common.icon, {marginRight:4, fontSize:20}]}>&#xe66c;</Text>
-                <Text style={{fontSize:16}}>循环列表[88]</Text>
-              </View>
+          <View style={{width:'100%', height:48, flexDirection:'row', borderTopLeftRadius:12, borderTopRightRadius:12, backgroundColor:'#444'}}>
+            <TouchableOpacity onPress={this.changePlayModel.bind(this)}>
+              {
+                this.state.playModel === 'loop' ?
+                  <View style={{width:138, height:'100%', flexDirection:'row', justifyContent:'center', alignItems:'center', borderTopLeftRadius:12}}>
+                    <Text style={[common.icon, {marginRight:4, fontSize:20, color:'#FFF'}]}>&#xe66c;</Text>
+                    <Text style={{fontSize:16, color:'#FFF'}}>循环列表[88]</Text>
+                  </View>
+                  : this.state.playModel === 'single-loop' ?
+                  <View style={{width:138, height:'100%', flexDirection:'row', justifyContent:'center', alignItems:'center', borderTopLeftRadius:12}}>
+                    <Text style={[common.icon, {marginRight:4, fontSize:20, color:'#FFF'}]}>&#xe66b;</Text>
+                    <Text style={{fontSize:16, color:'#FFF'}}>单曲循环[88]</Text>
+                  </View>
+                  :
+                  <View style={{width:138, height:'100%', flexDirection:'row', justifyContent:'center', alignItems:'center', borderTopLeftRadius:12}}>
+                    <Text style={[common.icon, {marginRight:4, fontSize:20, color:'#FFF'}]}>&#xe607;</Text>
+                    <Text style={{fontSize:16, color:'#FFF'}}>随机播放[88]</Text>
+                  </View>
+              }
             </TouchableOpacity>
             <View style={{flex:1, flexDirection:'row', justifyContent:'flex-end'}}>
               <TouchableOpacity>
                 <View style={{width:108, height:'100%', flexDirection:'row', justifyContent:'center', alignItems:'center'}}>
-                  <Text style={[common.icon, {fontSize:20}]}>&#xe80d;</Text>
-                  <Text style={{fontSize:16}}> 收藏全部</Text>
+                  <Text style={[common.icon, {fontSize:20, color:'#FFF'}]}>&#xe80d;</Text>
+                  <Text style={{fontSize:16, color:'#FFF'}}> 收藏全部</Text>
                 </View>
               </TouchableOpacity>
             </View>
             <TouchableOpacity>
               <View style={{width:46, height:'100%', justifyContent:'center', alignItems:'center'}}>
-                <Text style={[common.icon, {fontSize:20}]}>&#xe61f;</Text>
+                <Text style={[common.icon, {fontSize:20, color:'#FFF'}]}>&#xe61f;</Text>
               </View>
             </TouchableOpacity>
           </View>
-          <ScrollView style={{flex:1}}>
+          <ScrollView style={{flex:1, backgroundColor:'#FFF'}}>
             {
               temp.map((i, index) => {
                 return <Text>{i}</Text>
@@ -61,6 +75,18 @@ export default class NowPlayList extends React.Component {
     )
   }
 
+
+  changePlayModel () {
+    let type = 'loop';
+    if (this.state.playModel === 'loop') {
+      type = 'single-loop';
+    } else if (this.state.playModel === 'single-loop') {
+      type = 'random';
+    }
+    this.setState({
+      playModel: type
+    });
+  }
 }
 
 const styles = StyleSheet.create({
