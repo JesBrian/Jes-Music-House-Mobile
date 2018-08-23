f80568b113a53c1143c4f5ba5f6f89836626e1a7
JesBrian <JesBrian666@gmail.com>
2018-08-23 23:26:51 +0800

    歌单页 - [ 构建 update ]

--- src/pages/PlayList/PlayList.js
+++ src/pages/PlayList/PlayList.js
@@ -19,34 +19,34 @@ export default class PlayList extends React.Component {
       <View style={styles.container}>
         <PlayListNavbar />
 
-        <View>
+        <View style={{backgroundColor:'#666'}}>
           <View style={{height:138, backgroundColor:'#666'}}>
           </View>
 
-          <View style={{height:58, flexDirection:'row', backgroundColor:'#08eef0'}}>
+          <View style={{height:58, flexDirection:'row', backgroundColor:'#666'}}>
             <Ripple style={{flex:1, flexDirection:'column', justifyContent:'center', alignItems:'center'}}>
-              <Text>1</Text>
-              <Text>11</Text>
+              <Text style={[common.icon, {marginBottom:5, fontSize:22, color:'#FFF'}]}>&#xe638;</Text>
+              <Text style={{fontSize:13, color:'#DDD'}}>888</Text>
             </Ripple>
             <Ripple style={{flex:1, flexDirection:'column', justifyContent:'center', alignItems:'center'}}>
-              <Text>2</Text>
-              <Text>21</Text>
+              <Text style={[common.icon, {marginBottom:5, fontSize:22, color:'#FFF'}]}>&#xe615;</Text>
+              <Text style={{fontSize:13, color:'#DDD'}}>分享</Text>
             </Ripple>
             <Ripple style={{flex:1, flexDirection:'column', justifyContent:'center', alignItems:'center'}}>
-              <Text>3</Text>
-              <Text>31</Text>
+              <Text style={[common.icon, {marginBottom:5, fontSize:22, color:'#FFF'}]}>&#xe63c;</Text>
+              <Text style={{fontSize:13, color:'#DDD'}}>下载</Text>
             </Ripple>
             <Ripple style={{flex:1, flexDirection:'column', justifyContent:'center', alignItems:'center'}}>
-              <Text>4</Text>
-              <Text>41</Text>
+              <Text style={[common.icon, {marginBottom:5, fontSize:22, color:'#FFF'}]}>&#xe654;</Text>
+              <Text style={{fontSize:13, color:'#DDD'}}>多选</Text>
             </Ripple>
           </View>
 
           <View style={{width:'100%', height:48, flexDirection:'row', borderTopLeftRadius:12, borderTopRightRadius:12, backgroundColor:'#444'}}>
             <Ripple style={{flex:1}}>
               <View style={{height:'100%', padding:15, flex:1, flexDirection:'row', justifyContent:'flex-start', alignItems:'center', borderTopLeftRadius:12}}>
-                <Text style={[common.icon, {marginRight:4, fontSize:18, color:'#FFF'}]}>&#xe66c;</Text>
-                <Text style={{fontSize:16, color:'#FFF'}}>循环列表 [888]</Text>
+                <Text style={[common.icon, {marginRight:4, fontSize:18, color:'#FFF'}]}>&#xe6b4;</Text>
+                <Text style={{fontSize:16, color:'#FFF'}}>播放全部 [888]</Text>
               </View>
             </Ripple>
 
