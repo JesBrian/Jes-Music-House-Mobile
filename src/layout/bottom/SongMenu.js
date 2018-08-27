fdf18ae9691f6c9e0b22ee7ee9e0b5c3d7ee6a0e
JesBrian <JesBrian666@gmail.com>
2018-08-27 23:00:00 +0800

    菜单 - [ 构建 ]

--- src/layout/bottom/SongMenu.js
+++ src/layout/bottom/SongMenu.js
@@ -2,13 +2,20 @@ import React  from 'react';
 
 import {
   StyleSheet,
+  ScrollView,
   View,
+  Text,
   TouchableWithoutFeedback
 } from 'react-native';
 
 
 import { connect } from 'react-redux'
 import { hiddenBottomSongMenu } from '../../redux/actions/ViewActions.js'
+
+import Ripple from 'react-native-material-ripple'
+
+import { common } from '../../assets/styles/common.js'
+
 
 class SongMenu extends React.Component {
   constructor (props) {
@@ -19,10 +26,27 @@ class SongMenu extends React.Component {
 
   render () {
     return (
-      <View style={{width:'100%', height:'100%', bottom:0, left:0, flex:1, position:'absolute', backgroundColor:'rgba(0, 0, 0, 0.68)', zIndex:8}}>
+      <View style={{width:'100%', height:'100%', bottom:0, left:0, flex:1, position:'absolute', backgroundColor:'rgba(0, 0, 0, 0.68)', zIndex:18}}>
         <TouchableWithoutFeedback onPress={() => {this.props.dispatch(hiddenBottomSongMenu())}}>
           <View style={{flex:1}}/>
         </TouchableWithoutFeedback>
+
+        <View style={{width:'100%', height:338, position:'relative', flexDirection:'column'}}>
+          <View style={{width:'100%', height:33, justifyContent:'center', borderTopLeftRadius:12, borderTopRightRadius:12, backgroundColor:'#444'}}>
+            <Text style={{marginLeft:15, color:'#AAA'}}>歌曲：The Name of the Song</Text>
+          </View>
+
+          <ScrollView style={{flex:1, backgroundColor:'#FFF'}}>
+            <Ripple style={{height:33, flexDirection:'row', backgroundColor:'lightgreen'}}>
+              <View style={{width:38, justifyContent:'center', alignItems:'center'}}>
+                <Text style={[common.icon, {fontSize:21}]}>&#xe6b4;</Text>
+              </View>
+              <View style={{justifyContent:'center'}}>
+                <Text style={{fontSize:15}}>下一首播放</Text>
+              </View>
+            </Ripple>
+          </ScrollView>
+        </View>
       </View>
     )
   }
