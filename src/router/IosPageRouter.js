45f57cd81810d0ae1738d093230a531959f12b3a
JesBrian <JesBrian666@gmail.com>
2018-09-04 21:15:53 +0800

    页面架构 - [ 完善 ]

--- src/router/IosPageRouter.js
+++ src/router/IosPageRouter.js
@@ -8,8 +8,12 @@ import Index from '../pages/Index/IndexIos.js'
 import Search from '../pages/Search/Search.js'
 import Singer from '../pages/Singer/Singer.js'
 import User from '../pages/User/User.js'
-import PlayList from '../pages/PlayListDetail/PlayListDetail.js'
+import PlayList from '../pages/PlayList/PlayList.js'
+import PlayListDetail from '../pages/PlayListDetail/PlayListDetail.js'
+import Rank from '../pages/Rank/Rank.js'
+import Recommend from '../pages/Recommend/Recommend.js'
 import Comment from '../pages/Comment/Comment.js'
+import WriteTrend from '../pages/WriteTrend/WriteTrend.js'
 import AppTheme from '../pages/AppTheme/AppTheme.js'
 
 
@@ -23,8 +27,12 @@ const PageRouter = createStackNavigator({
   Singer: { screen: Singer },
   User: { screen: User },
   PlayList: { screen: PlayList },
+  PlayListDetail: { screen: PlayListDetail },
+  Rank: { screen: Rank },
+  Recommend: { screen: Recommend },
   Comment: { screen: Comment },
-  AppTheme: { screen: AppTheme }
+  WriteTrend: { screen: WriteTrend },
+  AppTheme: {screen: AppTheme}
 }, {
   initialRouteName: 'Index', // 默认显示界面
   headerMode: 'none', // 导航栏的显示模式, screen: 有渐变透明效果, float: 无透明效果, none: 隐藏导航栏
