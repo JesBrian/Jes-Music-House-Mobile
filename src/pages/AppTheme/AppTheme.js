798e28fe34cc760a35c0618d5a9122b9f1c4c328
JesBrian <JesBrian666@gmail.com>
2018-09-05 20:43:58 +0800

    主题页面 - [ 构建 again ]

--- src/pages/AppTheme/AppTheme.js
+++ src/pages/AppTheme/AppTheme.js
@@ -3,6 +3,7 @@ import React  from 'react';
 import {
   Dimensions,
   StyleSheet,
+  ScrollView,
   View,
   Text
 } from 'react-native';
@@ -11,9 +12,9 @@ import { connect } from 'react-redux'
 
 
 import Ripple from 'react-native-material-ripple'
+import LinearGradient from 'react-native-linear-gradient'
 
 import NormalNavbar from '../../layout/top/type/NormalNavbar.js'
-import SuperButton from '../../components/SuperButton.js'
 import SuperSlider from '../../components/SuperSlider.js'
 
 
@@ -21,8 +22,22 @@ class AppTheme extends React.Component {
   constructor (props) {
     super(props);
     this.state = {
-      colorSize: Dimensions.get('window').width * 0.93 * 0.17
+      colorSize: Dimensions.get('window').width * 0.93 * 0.17,
+      nowChooseColor: '#2e8ce6'
     };
+  }
+
+  componentWillMount () {
+    setTimeout(() => {
+      this.setState({
+        nowChooseColor: '#4cf0ab'
+      })
+    }, 5000)
+    setTimeout(() => {
+      this.setState({
+        nowChooseColor: '#ce7df0'
+      })
+    }, 10000)
   }
 
   render() {
@@ -32,30 +47,50 @@ class AppTheme extends React.Component {
       <View style={styles.container}>
         <NormalNavbar label={'个性换肤'} />
 
-        <View style={{flex:1, alignItems:'center', backgroundColor:'rgba(0,0,0,0.73)'}}>
-          <View style={{width:'90%', marginTop:28, flexDirection:'row', justifyContent:'space-around', flexWrap:'wrap'}}>
-            {
-              themeColorArr.map((item, index) => {
-                return (
-                  <Ripple key={index} style={{width:'17%', height:this.state.colorSize, marginBottom:9, position:'relative',borderRadius:8, backgroundColor:'lightgreen'}} >
-                    <View style={{width:12, height:12, top:3, right:3, position:'absolute', borderRadius:12, backgroundColor:'#333',}} />
-                  </Ripple>
-                )
-              })
-            }
-          </View>
+        <ScrollView style={{flex:1, backgroundColor:'rgba(0,0,0,0.73)'}}>
+          <View style={{flex:1, alignItems:'center'}}>
 
-          <View style={{width:'90%', marginTop:38}}>
-            <Text>调色盘</Text>
-            <View style={{height:58, marginTop:8, flexDirection:'row'}}>
-              <View style={{width:58, height:58, marginRight:15, marginBottom:9, position:'relative',borderRadius:8, backgroundColor:'lightgreen'}} />
-              <View style={{flex:1, justifyContent:'center'}}>
-                <SuperSlider />
-                <SuperSlider />
+            <View style={{width:'90%', height:this.state.colorSize * 2 + 38, marginTop:28}}>
+              <Text>预设主题色</Text>
+              <View style={{flex:1, flexDirection:'row', justifyContent:'space-between', flexWrap:'wrap'}}>
+                {
+                  themeColorArr.map((item, index) => {
+                    return (
+                      <Ripple key={index} style={{width:'17%', height:this.state.colorSize, marginBottom:9, position:'relative',borderRadius:8, backgroundColor:'lightgreen'}} >
+                        <View style={{width:12, height:12, top:3, right:3, position:'absolute', borderRadius:12, backgroundColor:'#333',}} />
+                      </Ripple>
+                    )
+                  })
+                }
               </View>
             </View>
+
+            <View style={{width:'90%', marginTop:38}}>
+              <Text>调色盘</Text>
+              <View style={{height:53, marginTop:8, flexDirection:'row'}}>
+                <View style={{width:53, height:53, marginRight:15, marginBottom:9, position:'relative',borderRadius:8, backgroundColor:'lightgreen'}} />
+                <View style={{flex:1, justifyContent:'center'}}>
+                  <LinearGradient start={{x: 0, y: 0}} end={{x: 1, y: 0}} colors={['#FF0000', '#FF7F00', '#FFFF00', '#00FF00', '#00FFFF', '#0000FF', '#8B00FF']} style={{width:'100%', height:10, marginTop:10, marginBottom:10, borderRadius:10}} />
+                  <LinearGradient start={{x: 0, y: 0}} end={{x: 1, y: 0}} colors={['#FFF', this.state.nowChooseColor, '#000']} style={{width:'100%', height:10, marginTop:10, marginBottom:10, borderRadius:10}} />
+                </View>
+              </View>
+            </View>
+
+            <View style={{width:'90%', marginTop:38}}>
+              <Text>模式切换</Text>
+              <View style={{marginTop:8, flexDirection:'row', justifyContent:'space-between'}}>
+                <Ripple style={{width:'45%', height:38, justifyContent:'center', alignItems:'center', borderRadius:5, backgroundColor:'lightgreen'}}>
+                  <Text>日间模式</Text>
+                </Ripple>
+                <Ripple style={{width:'45%', height:38, justifyContent:'center', alignItems:'center', borderRadius:5, backgroundColor:'lightgreen'}}>
+                  <Text>夜间模式</Text>
+                </Ripple>
+              </View>
+            </View>
+
           </View>
-        </View>
+        </ScrollView>
+
       </View>
     );
   }
@@ -63,6 +98,7 @@ class AppTheme extends React.Component {
 
 const styles = StyleSheet.create({
   container: {
+    height:'100%',
     flex:1,
     backgroundColor: 'transparent'
   },
