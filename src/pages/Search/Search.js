06ba92b09cbc988141caf90831293ceaa2134815
JesBrian <JesBrian666@gmail.com>
2018-10-06 17:54:29 +0800

    歌手分类 - [ update ]

--- src/pages/Search/Search.js
+++ src/pages/Search/Search.js
@@ -37,14 +37,14 @@ class Search extends React.Component {
         <SearchNavbar />
 
         <ScrollView style={{paddingTop:8, flex:1, backgroundColor:'rgba(0,0,0,0.73)'}}>
-          <Ripple onPress={() => {this.props.navigation.navigate('PlayList')}} style={{height:38, flexDirection:'row', alignItems:'center'}}>
+          <Ripple onPress={() => {this.props.navigation.navigate('Singer')}} style={{height:38, flexDirection:'row', alignItems:'center'}}>
             <View style={{height:'100%', paddingLeft:8, flex:1, flexDirection:'row', justifyContent:'center', alignItems:'center', borderBottomWidth:0.18, borderColor:'#888'}}>
               <SuperIcon type={'\ue671'} style={{color:this.props.color, fontSize:23}} />
               <Text style={{marginLeft:18, marginRight:18, color:'#DDD', fontSize:16}}>歌手分类</Text>
               <SuperIcon type={'\ue8cb'} style={{color:this.props.color, fontSize:23}} />
             </View>
           </Ripple>
-          <Ripple onPress={() => {this.props.navigation.navigate('Singer')}} style={{height:38, flexDirection:'row', alignItems:'center'}}>
+          <Ripple onPress={() => {this.props.navigation.navigate('PlayList')}} style={{height:38, flexDirection:'row', alignItems:'center'}}>
             <View style={{height:'100%', paddingLeft:8, flex:1, flexDirection:'row', justifyContent:'center', alignItems:'center', borderBottomWidth:0.18, borderColor:'#888'}}>
               <SuperIcon type={'\ue62b'} style={{color:this.props.color, fontSize:23}} />
               <Text style={{marginLeft:18, marginRight:18, color:'#DDD', fontSize:16}}>歌单分类</Text>
--- src/pages/Singer/Singer.js
+++ src/pages/Singer/Singer.js
@@ -6,38 +6,57 @@ import {
   Text
 } from 'react-native';
 
-import NormalNavbar from '../../layout/top/type/NormalNavbar.js'
-import SuperButton from '../../components/SuperButton.js'
+import { LargeList }  from 'react-native-largelist-v2'
+import Ripple from 'react-native-material-ripple'
 
+import NormalNavbar from '../../layout/top/type/NormalNavbar.js'
+
+
+const singerCategory = [
+  {items: [{name: '华语男歌手'}, {name: '华语女歌手'}, {name: '华语组合/乐队'}]},
+  {items: [{name: '欧美男歌手'}, {name: '欧美女歌手'}, {name: '欧美组合/乐队'}]},
+  {items: [{name: '日本男歌手'}, {name: '日本女歌手'}, {name: '日本组合/乐队'}]},
+  {items: [{name: '韩国男歌手'}, {name: '韩国女歌手'}, {name: '韩国组合/乐队'}]},
+  {items: [{name: '其他男歌手'}, {name: '其他女歌手'}, {name: '其他组合/乐队'}]}
+];
 export default class Singer extends React.Component {
   render() {
+
     return (
       <View style={styles.container}>
-        <NormalNavbar />
+        <NormalNavbar label={'歌手分类'} />
 
-        <Text onPress={()=>{this.openControlPanel()}} >歌手</Text>
-        <SuperButton label="登录" />
+        <LargeList style={{flex:1}}
+          data={singerCategory}
+          heightForSection={() => 18}
+          renderSection={this._renderSection}
+          heightForIndexPath={() => 45}
+          renderIndexPath={this._renderIndexPath}
+        />
 
-        <View style={styles.backgroundVideo} >
-        </View>
+
       </View>
     );
   }
+
+
+  _renderSection = () => {
+    return null;
+  };
+
+  _renderIndexPath = ({ section: section, row: row }) => {
+    return (
+      <Ripple style={{paddingLeft:23, flex:1, justifyContent:'center', backgroundColor:'rgba(0,0,0,0.3)'}}>
+        <Text style={{color:'#BBB'}}>{ singerCategory[section].items[row].name }</Text>
+      </Ripple>
+    );
+  };
 }
 
 const styles = StyleSheet.create({
   container: {
-    paddingTop:75,
-    backgroundColor: 'transparent'
-  },
-
-  backgroundVideo: {
-    width:108,
-    height:108,
-    top: 0,
-    left: 0,
-    bottom: 0,
-    right: 0,
-    backgroundColor:'#555'
-  },
+    paddingVertical:75,
+    flex:1,
+    backgroundColor:'rgba(0,0,0,0.73)'
+  }
 });
