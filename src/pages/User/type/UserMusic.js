bafe3b5f907cac8fcb07cfae74d280a26460c3e0
JesBrian <JesBrian666@gmail.com>
2018-10-09 11:06:27 +0800

    个人音乐页 - [ 完善 ]

--- src/pages/User/type/UserMusic.js
+++ src/pages/User/type/UserMusic.js
@@ -4,16 +4,18 @@ import {
   StyleSheet,
   View,
   Image,
-  Text,
-  TouchableWithoutFeedback
+  Text
 } from 'react-native'
 
+import { connect } from 'react-redux'
+
 import Ripple from 'react-native-material-ripple'
+import { withNavigation } from 'react-navigation'
 
 import SuperIcon from '../../../components/SuperIcon.js'
 
 
-export default class UserMusic extends React.Component {
+class UserMusic extends React.Component {
   render() {
     return (
       <View style={styles.container}>
@@ -26,27 +28,21 @@ export default class UserMusic extends React.Component {
         </View>
         <View>
           <View style={{height:53, flexDirection:'row'}}>
-            <Ripple onPress={() => {}} style={{flex:1, flexDirection:'row', alignItems:'center'}}>
+            <Ripple onPress={() => {this.props.navigation.navigate('PlayListDetail')}} style={{flex:1, flexDirection:'row', alignItems:'center'}}>
               <Image style={{width:42.5, height:42.5, marginLeft:6, marginRight:8, borderRadius:3}} source={{uri:'https://avatars3.githubusercontent.com/u/25942696?s=88&v=4'}} />
               <View style={{height:'100%', flex:1, justifyContent:'center', borderBottomWidth:0.18, borderColor:'#888'}}>
                 <Text style={{marginTop:5, marginBottom:3, fontSize:16, color:'#EEE'}}>我喜欢的音乐</Text>
                 <Text style={{fontSize:13, color:'#AAA'}}>180 首</Text>
               </View>
-            </Ripple>
-            <Ripple style={{width:43, height:'100%', justifyContent:'center', alignItems:'center', borderBottomWidth:0.18, borderColor:'#888'}}>
-              <SuperIcon type={'\ue653'} style={{fontSize:23, color:'#BBB'}} />
             </Ripple>
           </View>
           <View style={{height:53, flexDirection:'row'}}>
-            <Ripple onPress={() => {}} style={{flex:1, flexDirection:'row', alignItems:'center'}}>
+            <Ripple onPress={() => {this.props.navigation.navigate('PlayListDetail')}} style={{flex:1, flexDirection:'row', alignItems:'center'}}>
               <Image style={{width:42.5, height:42.5, marginLeft:6, marginRight:8, borderRadius:3}} source={{uri:'https://avatars3.githubusercontent.com/u/25942696?s=88&v=4'}} />
               <View style={{height:'100%', flex:1, justifyContent:'center', borderBottomWidth:0.18, borderColor:'#888'}}>
                 <Text style={{marginTop:5, marginBottom:3, fontSize:16, color:'#EEE'}}>我喜欢的音乐</Text>
                 <Text style={{fontSize:13, color:'#AAA'}}>180 首</Text>
               </View>
-            </Ripple>
-            <Ripple style={{width:43, height:'100%', justifyContent:'center', alignItems:'center', borderBottomWidth:0.18, borderColor:'#888'}}>
-              <SuperIcon type={'\ue653'} style={{fontSize:23, color:'#BBB'}} />
             </Ripple>
           </View>
         </View>
@@ -60,27 +56,21 @@ export default class UserMusic extends React.Component {
         </View>
         <View>
           <View style={{height:53, flexDirection:'row'}}>
-            <Ripple onPress={() => {}} style={{flex:1, flexDirection:'row', alignItems:'center'}}>
+            <Ripple onPress={() => {this.props.navigation.navigate('PlayListDetail')}} style={{flex:1, flexDirection:'row', alignItems:'center'}}>
               <Image style={{width:42.5, height:42.5, marginLeft:6, marginRight:8, borderRadius:3}} source={{uri:'https://avatars3.githubusercontent.com/u/25942696?s=88&v=4'}} />
               <View style={{height:'100%', flex:1, justifyContent:'center', borderBottomWidth:0.18, borderColor:'#888'}}>
                 <Text style={{marginTop:5, marginBottom:3, fontSize:16, color:'#EEE'}}>我喜欢的音乐</Text>
                 <Text style={{fontSize:13, color:'#AAA'}}>180 首</Text>
               </View>
-            </Ripple>
-            <Ripple style={{width:43, height:'100%', justifyContent:'center', alignItems:'center', borderBottomWidth:0.18, borderColor:'#888'}}>
-              <SuperIcon type={'\ue653'} style={{fontSize:23, color:'#BBB'}} />
             </Ripple>
           </View>
           <View style={{height:53, flexDirection:'row'}}>
-            <Ripple onPress={() => {}} style={{flex:1, flexDirection:'row', alignItems:'center'}}>
+            <Ripple onPress={() => {this.props.navigation.navigate('PlayListDetail')}} style={{flex:1, flexDirection:'row', alignItems:'center'}}>
               <Image style={{width:42.5, height:42.5, marginLeft:6, marginRight:8, borderRadius:3}} source={{uri:'https://avatars3.githubusercontent.com/u/25942696?s=88&v=4'}} />
               <View style={{height:'100%', flex:1, justifyContent:'center', borderBottomWidth:0.18, borderColor:'#888'}}>
                 <Text style={{marginTop:5, marginBottom:3, fontSize:16, color:'#EEE'}}>我喜欢的音乐</Text>
                 <Text style={{fontSize:13, color:'#AAA'}}>180 首</Text>
               </View>
-            </Ripple>
-            <Ripple style={{width:43, height:'100%', justifyContent:'center', alignItems:'center', borderBottomWidth:0.18, borderColor:'#888'}}>
-              <SuperIcon type={'\ue653'} style={{fontSize:23, color:'#BBB'}} />
             </Ripple>
           </View>
         </View>
@@ -97,3 +87,12 @@ const styles = StyleSheet.create({
     backgroundColor:'rgba(0,0,0,0.28)',
   },
 });
+
+
+function reduxState(store) {
+  return {
+    color: store.config.color
+  }
+}
+
+export default connect(reduxState)(withNavigation(UserMusic));
