70ba7fa38c0f513125d46ab25a762a5ca9d21e00
JesBrian <jesbrian666@gmail.com>
2018-11-24 22:41:17 +0800

    modal - [ update ]

--- src/layout/content/ContentModal/type/MultiSelectSong/MultiSelectSongModal.js
+++ src/layout/content/ContentModal/type/MultiSelectSong/MultiSelectSongModal.js
@@ -10,9 +10,10 @@ import { connect } from 'react-redux'
 import { hiddenModal } from '../../../../../redux/actions/ViewActions.js'
 
 import Ripple from 'react-native-material-ripple'
+import { LargeList } from 'react-native-largelist-v2'
 
 import MultiSelectNavbar from '../../../../top/TopNavbar/type/MultiSelectNavbar.js'
-import SuperIcon from "../../../../../components/SuperIcon/SuperIcon";
+import SuperIcon from '../../../../../components/SuperIcon/SuperIcon.js'
 
 class MultiSelectSongModal extends React.Component {
   constructor (props) {
@@ -24,35 +25,44 @@ class MultiSelectSongModal extends React.Component {
   render () {
     return (
       <View style={{flex:1, backgroundColor:'#888'}}>
-        <MultiSelectNavbar label={''} />
+        <MultiSelectNavbar />
 
         <ScrollView style={{flex:1, backgroundColor:'rgba(0,0,0,0.5)'}}>
-          <Text onPress={() => this.props.dispatch(hiddenModal())}>CheckboxSongModal</Text>
-          <Text onPress={() => this.props.dispatch(hiddenModal())}>CheckboxSongModal</Text>
-          <Text onPress={() => this.props.dispatch(hiddenModal())}>CheckboxSongModal</Text>
-          <Text onPress={() => this.props.dispatch(hiddenModal())}>CheckboxSongModal</Text>
-          <Text onPress={() => this.props.dispatch(hiddenModal())}>CheckboxSongModal</Text>
-          <Text onPress={() => this.props.dispatch(hiddenModal())}>CheckboxSongModal</Text>
-          <Text onPress={() => this.props.dispatch(hiddenModal())}>CheckboxSongModal</Text>
-          <Text onPress={() => this.props.dispatch(hiddenModal())}>CheckboxSongModal</Text>
-          <Text onPress={() => this.props.dispatch(hiddenModal())}>CheckboxSongModal</Text>
+          <LargeList style={{marginVertical:3, flex:1}}
+                     data={[{items: [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]}]}
+                     heightForIndexPath={() => 48}
+                     renderIndexPath={this._renderIndexPath}
+          />
         </ScrollView>
 
-        <View style={{height:70, flexDirection:'row'}}>
-          <Ripple style={{flex:1}}>
-            <SuperIcon />
-            <Text>1</Text>
+        <View style={{height:58, flexDirection:'row', backgroundColor:'#282828'}}>
+          <Ripple style={{flex:1, justifyContent:'center', alignItems:'center'}}>
+            <SuperIcon type={'\ue66c'} style={{fontSize:26, color:'#BBB'}} />
+            <Text style={{color:'#BBB'}}>下一首播放</Text>
           </Ripple>
-          <Ripple style={{flex:1}}>
-            <SuperIcon />
-            <Text>1</Text>
+          <Ripple style={{flex:1, justifyContent:'center', alignItems:'center'}}>
+            <SuperIcon type={'\ue654'} style={{fontSize:26, color:'#BBB'}} />
+            <Text style={{color:'#BBB'}}>加入歌单</Text>
           </Ripple>
-          <Ripple style={{flex:1}}>
-            <SuperIcon />
-            <Text>1</Text>
+          <Ripple style={{flex:1, justifyContent:'center', alignItems:'center'}}>
+            <SuperIcon type={'\ue63c'} style={{fontSize:26, color:'#BBB'}} />
+            <Text style={{color:'#BBB'}}>下载所选</Text>
           </Ripple>
         </View>
       </View>
+    );
+  }
+
+  _renderIndexPath  = () => {
+    return (
+      <Ripple style={{height:48, marginLeft:3, paddingLeft: 3, flex: 1, flexDirection: 'row', alignItems: 'center'}}>
+        <View style={{width:38, height:'100%'}}>
+        </View>
+        <View style={{height:'100%', paddingLeft:3, flex:1, justifyContent:'center', borderBottomWidth:0.2, borderColor: '#FFF'}}>
+          <Text style={{fontSize: 15, color:'#CCC'}}>555长cjk沙健id45康</Text>
+          <Text style={{fontSize: 12, color:'#888'}}> - JesBrian</Text>
+        </View>
+      </Ripple>
     );
   }
 }
