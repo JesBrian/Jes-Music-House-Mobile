af5c86f3a12d48ffb2d7f8217890de1f2c518ddf
JesBrian <JesBrian666@gmail.com>
2018-11-27 23:01:37 +0800

    music player - [ import swipe plugins ]

--- src/layout/bottom/BottomMusicPlayer/MusicPlayer.js
+++ src/layout/bottom/BottomMusicPlayer/MusicPlayer.js
@@ -5,8 +5,7 @@ import {
   StyleSheet,
   View,
   Text,
-  Image,
-  TouchableOpacity
+  Image
 } from 'react-native';
 
 
@@ -14,8 +13,9 @@ import { connect } from 'react-redux'
 import { showBottomPlayList } from '../../../redux/actions/ViewActions.js'
 import { changeMusicPlayStatus } from '../../../redux/actions/MusicActions.js'
 
-import { AnimatedCircularProgress } from 'react-native-circular-progress'
+import Swiper from 'react-native-swiper'
 import Ripple from 'react-native-material-ripple'
+import { AnimatedCircularProgress } from 'react-native-circular-progress'
 import { BoxShadow }  from 'react-native-shadow'
 
 import { common } from '../../../assets/styles/common.js'
@@ -42,17 +42,26 @@ class MusicPlayer extends React.Component {
       <View style={{width:Dimensions.get('window').width, height:65, top:Dimensions.get('window').height - (this.props.isShow === false ? 0 : 65), left:0, paddingTop:12, position:'absolute', zIndex:5}}>
         <BoxShadow setting={shadowOpt}>
           <View style={{flex:1, flexDirection:'row', backgroundColor:this.props.config.theme === 'light' ? '#F8F8F8' : '#282828'}}>
-            <TouchableOpacity onPress={() => {this.props.config.navigation.navigate('Song');}} style={{flex:1, flexDirection:'row', justifyContent:'flex-start'}}>
-              <View style={{width:58, flexDirection:'row', alignItems:'center', justifyContent:'center' }}>
-                <TouchableOpacity onPress={() => {this.props.config.navigation.navigate('Song');}} style={{width:'76%', height:'76%', marginLeft:2}}>
-                  <Image style={{flex:1, marginLeft:2, borderWidth:1, borderRadius:4, borderColor:'#666'}} source={{uri:'https://avatars3.githubusercontent.com/u/25942696?s=88&v=4'}} />
-                </TouchableOpacity>
-              </View>
-              <View style={{flex:1, flexDirection:'column', justifyContent:'center', alignItems:'flex-start'}}>
-                <Text style={{textAlign:'left', fontSize:15, color:'#CCC'}}>The Name of the Song</Text>
-                <Text style={{textAlign:'left', fontSize:13, color:'#AAA'}}>JesBrianj 苏敬雄</Text>
-              </View>
-            </TouchableOpacity>
+            <Swiper loop={true} showButtons={false} showsPagination={false} >
+              <Ripple onPress={() => {this.props.config.navigation.navigate('Song');}} style={{flex:1, flexDirection:'row', justifyContent:'flex-start'}}>
+                <View style={{width:58, flexDirection:'row', alignItems:'center', justifyContent:'center' }}>
+                  <Image style={{width:38, height:38, marginLeft:2, borderWidth:1, borderRadius:4, borderColor:'#666'}} source={{uri:'https://avatars3.githubusercontent.com/u/25942696?s=88&v=4'}} />
+                </View>
+                <View style={{flex:1, flexDirection:'column', justifyContent:'center', alignItems:'flex-start'}}>
+                  <Text style={{textAlign:'left', fontSize:15, color:'#CCC'}}>The Name of the Song</Text>
+                  <Text style={{textAlign:'left', fontSize:13, color:'#AAA'}}>JesBrianj 苏敬雄</Text>
+                </View>
+              </Ripple>
+              <Ripple onPress={() => {this.props.config.navigation.navigate('Song');}} style={{flex:1, flexDirection:'row', justifyContent:'flex-start'}}>
+                <View style={{width:58, flexDirection:'row', alignItems:'center', justifyContent:'center' }}>
+                  <Image style={{width:38, height:38, marginLeft:2, borderWidth:1, borderRadius:4, borderColor:'#666'}} source={{uri:'https://avatars3.githubusercontent.com/u/25942696?s=88&v=4'}} />
+                </View>
+                <View style={{flex:1, flexDirection:'column', justifyContent:'center', alignItems:'flex-start'}}>
+                  <Text style={{textAlign:'left', fontSize:15, color:'#CCC'}}>The Name of the Song</Text>
+                  <Text style={{textAlign:'left', fontSize:13, color:'#AAA'}}>JesBrianj 苏敬雄</Text>
+                </View>
+              </Ripple>
+            </Swiper>
             <View style={{width:88, flexDirection:'row', justifyContent:'center'}}>
               <Ripple onPress={this.changePlayStatus} style={{paddingHorizontal:10, flexDirection:'column', justifyContent:'center'}}>
 
@@ -69,9 +78,9 @@ class MusicPlayer extends React.Component {
                   }
                 </AnimatedCircularProgress>
               </Ripple>
-              <TouchableOpacity onPress={() => {this.props.dispatch(showBottomPlayList())}} style={{paddingLeft:8, paddingRight:18, flexDirection:'column', justifyContent:'center'}}>
+              <Ripple onPress={() => {this.props.dispatch(showBottomPlayList())}} style={{paddingLeft:8, paddingRight:18, flexDirection:'column', justifyContent:'center'}}>
                 <SuperIcon type={'\ue654'} style={{fontSize:26, color:'#CCC'}} />
-              </TouchableOpacity>
+              </Ripple>
             </View>
           </View>
         </BoxShadow>
